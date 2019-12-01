a08f795b13eec704bc976f59f12c4a785f8f5a2d
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-01 12:18:03 +0200

    day1 - use `read_to_string`

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,17 +3,14 @@ use day1::{part_1, part_2};
 use std::io::{stdin, Read};
 
 fn main() -> Result<()> {
-    let mut input = vec![];
+    let mut input = String::new();
 
     stdin()
-        .read_to_end(&mut input)
+        .read_to_string(&mut input)
         .context("Failed to read input from stdin")?;
 
-    let input_as_string =
-        String::from_utf8(input).context("String contains Non-UTF8 characters")?;
-
-    println!("Part 1 - {}", part_1(&input_as_string)?);
-    println!("Part 2 - {}", part_2(&input_as_string)?);
+    println!("Part 1 - {}", part_1(&input)?);
+    println!("Part 2 - {}", part_2(&input)?);
 
     Ok(())
 }
