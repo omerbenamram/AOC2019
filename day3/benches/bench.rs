e84732a1447646c5a16c2d1e6f6d4fe025eb9f7f
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-03 11:27:19 +0200

    update bench

--- day3/benches/bench.rs
+++ day3/benches/bench.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs;
 
 fn benchmark(c: &mut Criterion) {
-    let s = fs::read_to_string(r#"input\opcodes"#).unwrap();
+    let s = fs::read_to_string(r#"input\wires"#).unwrap();
 
     c.bench_function("2019 day 3 part one", |b| {
         b.iter(|| day3::part_1(&s));
