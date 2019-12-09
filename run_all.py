2e245d80a9439d6e81bca590566e3e37bc2e41fc
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-09 13:49:24 +0200

    use glob

--- run_all.py
+++ run_all.py
@@ -1,9 +1,8 @@
+import glob
 import os
 import subprocess
-from pathlib import Path
 
 if __name__ == '__main__':
-    for (r, d, files) in os.walk(Path('.')):
-        for f in files:
-            if f.endswith('.ps1'):
-                subprocess.run(["powershell", "-NoProfile", f".\{f}"], cwd=r)
+    for p in glob.glob(r"**\*.ps1"):
+        root, base = os.path.dirname(p), os.path.basename(p)
+        subprocess.run(["powershell", "-NoProfile", f".\{base}"], cwd=root)
