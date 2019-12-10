fed14bbcce1c3738becb6d232e928d6d53461e6a
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-10 15:16:39 +0200

    day10 -part1

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -25,38 +25,19 @@ fn parse_input(input: &str) -> Vec<Coord> {
         .collect()
 }
 
-#[test]
-fn test_parse_input() {
-    assert_eq!(
-        parse_input(
-            "\
-    .#..#
-.....
-#####
-....#
-...##"
-        ),
-        vec![
-            (1, 0),
-            (4, 0),
-            (0, 2),
-            (1, 2),
-            (2, 2),
-            (3, 2),
-            (4, 2),
-            (4, 3),
-            (3, 4),
-            (4, 4)
-        ]
-    )
-}
-
 fn distance(a: Coord, b: Coord) -> f32 {
     (((a.0 - b.0).pow(2) + (a.1 - b.1).pow(2)) as f32).sqrt()
 }
 
+fn angle(a: Coord, b: Coord) -> f32 {
+    let dx = (a.0 - b.0) as f32;
+    let dy = (a.1 - b.1) as f32;
+    dx.atan2(dy)
+}
+
 pub fn part_1(input: &str) -> Result<usize> {
     let astroids = parse_input(input);
+
     if astroids.is_empty() {
         bail!("Input is empty.");
     }
@@ -75,33 +56,12 @@ pub fn part_1(input: &str) -> Result<usize> {
             }
 
             // We need to take care of special case of `0` (where slope is 0 but both left and right need to be considered)
-            match (astroid.1 - another.1) as f32 / (astroid.0 - another.0) as f32 {
-                0.0 => {
-                    if (astroid == (11, 13) || another == (11, 13)) {
-                        debug!("DX: {:?} {:?}", another, astroid);
-                    }
-                    // say we have (3,2) and (4,2)
-                    edges_to_slope.insert((astroid, another), 0.0001);
-                    edges_to_slope.insert((another, astroid), -1.0 * 0.0001);
-                }
-                f if f.is_infinite() && f.is_sign_positive() => {
-                    edges_to_slope.insert((astroid, another), 1000.0);
-                    edges_to_slope.insert((another, astroid), -1.0 * 1000.0);
-                }
-                f if f.is_infinite() && f.is_sign_negative() => {
-                    edges_to_slope.insert((astroid, another), -1.0 * 1000.0);
-                    edges_to_slope.insert((another, astroid), 1000.0);
-                }
-                slope => {
-                    if (astroid == (11, 13) || another == (11, 13)) {
-                        debug!("{:?} {:?} -> {}", another, astroid, slope);
-                    }
-                    edges_to_slope.insert((another, astroid), slope);
-                    edges_to_slope.insert((astroid, another), slope);
-                }
-            };
+            let angle = angle(astroid, another);
             graph.add_edge(astroid, another);
+            edges_to_slope.insert((astroid, another), angle);
+
             graph.add_edge(another, astroid);
+            edges_to_slope.insert((another, astroid), std::f32::consts::PI - angle);
         }
     }
 
@@ -121,9 +81,6 @@ pub fn part_1(input: &str) -> Result<usize> {
             .group_by(|&another| edges_to_slope.get(&(vertex, another)).expect("inserted"))
         {
             let edges = edges.collect::<Vec<Coord>>();
-            if vertex == (11, 13) {
-                debug!("{:?} - slope {} edges {:?}", vertex, slope, &edges);
-            }
 
             let closest = edges
                 .into_iter()
@@ -131,23 +88,8 @@ pub fn part_1(input: &str) -> Result<usize> {
                 .min_by(|edge_1, edge_2| edge_1.1.partial_cmp(&edge_2.1).unwrap_or(Ordering::Less))
                 .context("A minimum should exist")?;
 
-            if vertex == (11, 13) {
-                debug!(
-                    "{:?} - closest is {:?} with slope {}, distance {}",
-                    vertex, closest.0, slope, closest.1
-                );
-            }
-
             visibility_graph.add_edge(vertex, closest.0);
             visibility_graph.add_edge(closest.0, vertex);
-        }
-    }
-
-    if let Some(edges) = visibility_graph.edges(&(11, 13)) {
-        for vertex in astroids {
-            if !edges.contains(&vertex) {
-                println!("{:?}", vertex);
-            }
         }
     }
 
@@ -165,6 +107,36 @@ pub fn part_2(input: &str) -> Result<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn test_parse_input() {
+        assert_eq!(
+            parse_input(
+                "\
+    .#..#
+.....
+#####
+....#
+...##"
+            ),
+            vec![
+                (1, 0),
+                (4, 0),
+                (0, 2),
+                (1, 2),
+                (2, 2),
+                (3, 2),
+                (4, 2),
+                (4, 3),
+                (3, 4),
+                (4, 4)
+            ]
+        )
+    }
+
+    #[test]
+    fn test_angle() {
+        assert_eq!(angle((11, 13), (11, 17)), angle((11, 13), (11, 14)));
+    }
 
     #[test]
     fn test_part1_210() {
