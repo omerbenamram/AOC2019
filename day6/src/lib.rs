d8c1c644eab41e5885397bfb0d73f1c96de2197c
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-10 21:06:11 +0200

    playing with allocations

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -18,28 +18,34 @@ impl Graph {
         }
     }
 
+    pub fn with_capacity(n: usize) -> Self {
+        Graph {
+            adjacency_list: HashMap::with_capacity(n),
+        }
+    }
+
     pub fn add_edge(&mut self, from: Vertex, to: Vertex) {
         self.adjacency_list.entry(from).or_insert(vec![]).push(to);
     }
 
     /// Returns a map of paths from vertex `start` to each other vertex in the graph.
     /// The key is the shortest path length.
-    pub fn bfs(&self, start: Vertex) -> HashMap<u32, HashSet<Vertex>> {
+    pub fn bfs<'a>(&'a self, start: &'a str) -> HashMap<u32, HashSet<&'a str>> {
         let mut queue = VecDeque::new();
-        queue.push_back(&start);
+        queue.push_back(start);
 
         // Although the graph in this question should not contain loops, better safe than sorry.
-        let mut visited = HashSet::new();
-        visited.insert(&start);
+        let mut visited = HashSet::with_capacity(self.adjacency_list.len());
+        visited.insert(start);
 
         // Extra bookeeping to allow ourselves to keep track of depth
         // while getting away with using a queue for BFS (instead of "list of lists")
         let mut node_to_depth = HashMap::new();
-        node_to_depth.insert(&start, 0);
+        node_to_depth.insert(start, 0);
 
         let mut layers = HashMap::new();
-        let mut h = HashSet::new();
-        h.insert(start.clone());
+        let mut h: HashSet<&str> = HashSet::new();
+        h.insert(start);
         layers.insert(0, h);
 
         while !queue.is_empty() {
@@ -48,17 +54,17 @@ impl Graph {
 
             if let Some(neighbors) = self.adjacency_list.get(v) {
                 for neighbor in neighbors.iter() {
-                    if !visited.contains(&neighbor) {
+                    if !visited.contains(neighbor.as_str()) {
                         let parent_depth = node_to_depth.get(v).expect("parent must exist").clone();
                         let this_depth = parent_depth + 1;
-                        node_to_depth.insert(neighbor, this_depth);
+                        node_to_depth.insert(&neighbor, this_depth);
                         layers
                             .entry(this_depth)
                             .or_insert_with(HashSet::new)
-                            .insert(neighbor.clone());
+                            .insert(neighbor);
 
-                        visited.insert(neighbor);
-                        queue.push_back(neighbor)
+                        visited.insert(neighbor.as_str());
+                        queue.push_back(neighbor.as_str())
                     }
                 }
             }
@@ -86,7 +92,7 @@ pub fn part_1(input: &str) -> Result<u32> {
         g.add_edge(edge[0].to_owned(), edge[1].to_owned());
     }
 
-    let bfs = g.bfs(CENTER_OF_MASS.to_string());
+    let bfs = g.bfs(CENTER_OF_MASS);
     let mut total_orbits = 0;
 
     // we just count how many orbits are at each distance from `COM`
@@ -114,10 +120,10 @@ pub fn part_2(input: &str) -> Result<u32> {
         g.add_edge(edge[1].to_owned(), edge[0].to_owned());
     }
 
-    let bfs = g.bfs("YOU".to_string());
+    let bfs = g.bfs("YOU");
 
     for (len, vertexes) in bfs {
-        if vertexes.contains(&"SAN".to_string()) {
+        if vertexes.contains("SAN") {
             return Ok(len - 2);
         }
     }
